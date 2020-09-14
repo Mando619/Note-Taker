0b31e85c98dc68647d42d22df9e6d95901093e94
Armando Estrada <libryum33@hotmail.com>
2020-09-13 17:04:12 -0700

    finished seting up html routes. now to work on the api

---
+++ routes/apiRoutes.js
@@ -0,0 +1,14 @@
+//var path = require("path");
+
+// app.get("/api/notes/:notes", function(req, res) {
+   // var newNotes = req.params.notes
+
+  //  console.log(newNotes)
+ // });
+  
+  
+//app.post("/api/notes/", function(req, res) {
+   // var newNotes = req.body; 
+  //  console.log(newNotes)
+
+//});
--- server.js
+++ server.js
@@ -1,23 +1,22 @@
 
 // these are the dependencies 
 var express = require("express");
-//var path = require("path");
-//var fs =require("fs");
 
 var app = express();
 
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 // this helps express app to handle JSON data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-
+app.use(express.static("public"));
+app.use(express.static("db"));
 
 
   //require("./routes/apiRoutes")(app);
-  require("./routes/htmlRoutes")(app);
+  require("./routes/htmlRoutes.js")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
