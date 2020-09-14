f7cc5f3d44772b22a47ff9b1d81e52623df8c5b4
Armando Estrada <libryum33@hotmail.com>
2020-09-13 18:23:17 -0700

    work on deleting data next.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,14 +1,38 @@
-//var path = require("path");
 
-// app.get("/api/notes/:notes", function(req, res) {
-   // var newNotes = req.params.notes
 
-  //  console.log(newNotes)
- // });
-  
-  
-//app.post("/api/notes/", function(req, res) {
-   // var newNotes = req.body; 
-  //  console.log(newNotes)
 
-//});
+
+module.exports = function (app) {
+
+   // using JSON.parse to convert the text into a JavaScript object
+   app.get("/api/notes/", function (req, res) {
+      fs.readFile("db/db.json", "utf8", function (err, data) {
+         if (err) throw err
+         var newNotes = JSON.parse(data);
+         res.json(newNotes);
+      })
+   });
+
+   app.get('*', function (req, res) {
+      res.sendFile(path.join(__dirname, "public/index.html"));
+   });
+
+   app.post("/api/notes/", function (req, res) {
+      var newNotes = req.body
+      console.log(newNotes)
+      fs.readFile("db/db.json", "utf8", function (err, data) {
+         console.log(newNotes)
+         if (err) throw err
+         var noteData = JSON.parse(data)
+         // adding ID to each not when recorded
+         noteData.push(newNote);
+         noteDatae.forEach((item, i) => item.id = i + 1);
+         console.log(noteData);
+
+         fs.writeFile("db/db.json", JSON.stringify(noteDatae), "utf8", function (err) {
+            if (err) throw err
+            console.log("Finished writing file")
+         })
+      })
+   });
+};
